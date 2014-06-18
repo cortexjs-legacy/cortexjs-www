5d9eaedac1d903678ee1050bed9e5560eb37cca5
ltebean <yucong1118@gmail.com>
2014-06-18 18:01:35 +0800

    use cortex

--- public/gulpfile.js
+++ public/gulpfile.js
@@ -29,11 +29,17 @@ gulp.task('img', function() {
 		.pipe(gulp.dest("./build/img"));
 });
 
+gulp.task('cortex', function() {
+	gulp.src(["./dev/js/neurons/**/*"])
+		.pipe(gulp.dest("./build/neurons"));
+});
+
 
 gulp.task('watch', function() {
 	// gulp.watch(["./dev/views/**/*.jade"], ['jade']);
 	gulp.watch(["./dev/css/**/*.styl"], ['stylus']);
+	gulp.watch(["./dev/js/**/*"], ['cortex']);
 });
 
-gulp.task('default', ['stylus',  'img' ,'watch']);
-gulp.task('build', ['stylus','img']);
+gulp.task('default', ['stylus', 'img' ,'cortex', 'watch']);
+gulp.task('build', ['stylus','img','cortex']);
