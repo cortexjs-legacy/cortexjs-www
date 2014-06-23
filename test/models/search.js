faad624d562a6da6e9073a421958a26c3061a34d
Wei Gao <jky239@gmail.com>
2014-06-23 18:52:30 +0800

    elasticsearch for keywords and name

--- test/models/search.js
+++ test/models/search.js
@@ -1,9 +1,18 @@
 var Search = require('../../lib/models/search');
 
 describe('model/search', function() {
-  it.only('search', function(done) {
+
+  it('search', function(done) {
     Search.search('villadora', function(err, res) {
       console.log(res, res.length);
+      done(err);
+    });
+  });
+
+
+  it.only('searchByKeyword', function(done) {
+    Search.searchByKeyword(['fx', 'color'], function(err, res) {
+      console.log(err, res[0].versions);
       done(err);
     });
   });
