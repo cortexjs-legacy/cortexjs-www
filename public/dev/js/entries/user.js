d3ad8b5e59d1e3625b3c02a3d90e931ae9307f58
Wei Gao <jky239@gmail.com>
2014-06-29 14:26:55 +0800

    add zeroclipboard to install command

--- public/dev/js/entries/user.js
+++ public/dev/js/entries/user.js
@@ -1,16 +1,28 @@
 var nav = require('../lib/nav');
 var $ = require('zepto');
 
+var ZeroClipboard = require('zeroclipboard');
+
+var client = new ZeroClipboard($(".copy-button"));
+
+client.on('ready', function(event) {
+
+	client.on('aftercopy', function(event) {
+		alert('Copied to clipboard');
+	});
+});
+
+
 nav.init();
 var followBtn = $('.btn-follow');
 var user = followBtn.data('user');
 var followed = followBtn.data('followed');
-var followers=$('.followers');
+var followers = $('.followers');
 
 followBtn.on('click', function() {
-	if(followBtn.data('followed')==0){
+	if (followBtn.data('followed') == 0) {
 		follow()
-	}else{
+	} else {
 		unfollow();
 	}
 })
@@ -25,8 +37,8 @@ function follow() {
 		contentType: 'application/json',
 		success: function() {
 			followBtn.html('UNFOLLOW')
-			followBtn.data('followed','1');
-			followers.html(Number.parseInt(followers.html())+1);
+			followBtn.data('followed', '1');
+			followers.html(Number.parseInt(followers.html()) + 1);
 		},
 		error: function(xhr, type) {
 			console.log('error')
@@ -48,8 +60,8 @@ function unfollow() {
 		contentType: 'application/json',
 		success: function() {
 			followBtn.html('FOLLOW')
-			followBtn.data('followed','0')
-			followers.html(Number.parseInt(followers.html())-1);
+			followBtn.data('followed', '0')
+			followers.html(Number.parseInt(followers.html()) - 1);
 		},
 		error: function(xhr, type) {
 			console.log('error')
