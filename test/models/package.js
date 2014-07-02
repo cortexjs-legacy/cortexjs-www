ce04ab6acdf159a6d972683c09545e7323a3e9ed
Wei Gao <jky239@gmail.com>
2014-07-02 21:05:09 +0800

    add unit test

--- test/models/package.js
+++ test/models/package.js
@@ -1,5 +1,15 @@
-var Package = require('../../lib/models/package');
+var Packages = require('../../lib/models/package');
+var moment = require('moment');
 
 describe('model/package', function() {
+	it('recentlyUpdated', function(done) {
+		Packages.recentlyUpdated(function(err, pkgs) {
+			console.log(err, pkgs.map(function(pkg) {
+				pkg.updated = moment(pkg.updated).fromNow();
+				return pkg;
+			}));
 
+			done(err);
+		});
+	});
 });
