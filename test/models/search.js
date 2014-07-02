7c47146e1ca2bccfd09eb3972f595c0d62c74705
Wei Gao <jky239@gmail.com>
2014-07-02 17:28:52 +0800

    search '*' with no query parameter proided

--- test/models/search.js
+++ test/models/search.js
@@ -2,6 +2,13 @@ var Search = require('../../lib/models/search');
 
 describe('model/search', function() {
 
+  it.only('empty search', function(done) {
+    Search.search(function(err, res) {
+      console.log(res, res.length);
+      done(err);
+    });
+  });
+
   it('search', function(done) {
     Search.search('villadora', function(err, res) {
       console.log(res, res.length);
@@ -10,7 +17,7 @@ describe('model/search', function() {
   });
 
 
-  it.only('searchByKeyword', function(done) {
+  it('searchByKeyword', function(done) {
     Search.searchByKeyword(['fx', 'color'], function(err, res) {
       console.log(err, res[0].versions);
       done(err);
